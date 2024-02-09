4313d4aa5b50dc281b526bd526c7e6fe96f4d82a
Philipp Bleimund <bleimund.philipp@gmail.com>
2024-02-09 14:31:11 +0100

    Added exit case and added comments

--- src/userInterface.c
+++ src/userInterface.c
@@ -1,61 +1,73 @@
 #include <linux/limits.h>
+#include <ncurses.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#include <ncurses.h>
-
-
 
 #define printf printw
 
-
+/* Struct used for storing multiple file links
+ */
 typedef struct {
   FILE fileLink;
   char path[PATH_MAX];
   char name[PATH_MAX];
 } file_t;
 
+/* Global Variables
+ */
+
 file_t currentDatabaseFile[20];
 int numberDatabases = 0;
 
 char default_str[17] = "nothing selected";
 
-char* getDatabaseName(int index){
+/* Helper functions
+ */
 
-  if(numberDatabases == 0){
+char *getDatabaseName(int index) {
+
+  if (numberDatabases == 0) {
     return default_str;
-  }else{
-     return currentDatabaseFile[index].name;
- }
+  } else {
+    return currentDatabaseFile[index].name;
+  }
 }
 
+/* Main method
+ */
 
-int main(void){
+int main(void) {
 
   printf("test1\n");
 
   initscr();
   scrollok(stdscr, TRUE);
 
-  while (true){
+  bool keeprunning = true;
+
+  while (keeprunning) {
     clear();
     printf("======status=======\n");
-    printf("current selected database: %s", getDatabaseName(0));
-    
+    printf("current selected database: %s\n", getDatabaseName(0));
+
     char c = getch();
-    if (c == EOF)
-      printf("error try again");
+    if (c == EOF) {
+      printf("error try again\npress any key to continue...");
+      getch();
+    }
 
     switch (c) {
-      case 'a':
-        printf("Youve entered the char a");
+    case 'a':
+      printf("Youve entered the char a");
+    case 'd':
+      keeprunning = false;
     }
 
     getch();
   }
-  
+
   endwin();
 
   return EXIT_SUCCESS;
 }
-
